4588d9cdeb0911d10bd27952c1163a5435526a7f
islaturrahman <islhadevelopers@gmail.com>
2022-12-06 13:19:28 +0700

    add file PKM-K UNAS

--- src/config.js
+++ src/config.js
@@ -1,3 +1,12 @@
 import { async } from "../node_modules/@firebase/util";
 import { getAuth } from "../node_modules/firebase/auth";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBikWzrGOdpJrfRFBKh_7KeQF6YwuY751k",
+    authDomain: "myblog-ff8f8.firebaseapp.com",
+    projectId: "myblog-ff8f8",
+    storageBucket: "myblog-ff8f8.appspot.com",
+    messagingSenderId: "280074652987",
+    appId: "1:280074652987:web:d61cf5f46c0c6f9a3a3b1e",
+    measurementId: "G-MVGD2E8CJY"
+};
